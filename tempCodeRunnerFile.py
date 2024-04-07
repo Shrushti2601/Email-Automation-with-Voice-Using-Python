de4d6a41a0199758edf031e35cbfced60d0b70a4
Shrushti2601 <116190590+Shrushti2601@users.noreply.github.com>
2024-04-07 23:40:47 +0530

    Add files via upload
    Email automation with voice using Python involves creating a system where users can dictate emails verbally, and the Python script translates the spoken words into text, then sends the email based on the dictated content. import smtplib  # Allows sending emails using SMTP (Simple Mail Transfer Protocol)
    import speech_recognition as sr  # Provides speech recognition functionality for converting spoken words to text
    import pyttsx3  # Enables text-to-speech conversion, allowing the system to provide spoken feedback
    from email.message import EmailMessage  # Provides EmailMessage class for composing emails 
    These libraries are essential for various tasks within the script, such as sending emails, transcribing spoken words into text, converting text into spoken words, and composing email messages.

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,41 @@
+import smtplib
+import speech_recognition as sr
+from email.message import EmailMessage
+import pyttsx3
+listenser=sr.Recognizer()
+tts=pyttsx3.Engine()
+
+def talking_tom(text):
+    tts.say(text)
+    tts.runAndWait()
+    
+def mic():
+    with sr.Microphone() as source:
+        print("program is listening.....") 
+        voice=listenser.listen(source)
+        data=listenser.recognize_google(voice)
+        print(data) 
+        return data.lower()
+dict={"shrushti":"anandgandhamwar2009@gmail.com"}
+def send_mail(receiver,subject,body):
+    server=smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login("shrushtimahajan2601@gmail.com","ezxy fuwr etdi rosx")
+    email=EmailMessage()
+    email["From"]="shrushtimahajan2601@gmail.com"
+    email["To"]=receiver
+    email["Subject"]=subject
+    email.set_content(body)
+    server.send_message(email)
+    
+def main_poc():
+    talking_tom("To whom do you want to send")
+    name=mic()
+    receiver=dict[name]
+    talking_tom("speak subject")
+    subject=mic()
+    talking_tom("speak msg of email")
+    body=mic()
+    send_mail(receiver,subject,body)
+    print("your mail has been sent!")
+main_poc()
